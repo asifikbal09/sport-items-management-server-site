3aebe25c05ecf44cbb653aa6feec55078728e207
asifikbal09 <asif.ikbal662@gmail.com>
2024-02-09 00:23:45 +0600

    [DONE] Product validation complete

--- src/app/modules/product/product.validation.ts
+++ src/app/modules/product/product.validation.ts
@@ -1,9 +1,31 @@
 import { z } from 'zod';
 
-const productValidationSchema = z.object({
-    
+const createProductValidationSchema = z.object({
+  body: z.object({
+    name: z.string({ required_error: 'Name is required.' }),
+    price: z.number({ required_error: 'Price is required.' }),
+    quantity: z.number({ required_error: 'Price is required.' }),
+    types: z.string(),
+    brand: z.string(),
+    size: z.enum(['small', 'medium', 'large']),
+    color: z.string(),
+    condition: z.enum(['new', 'used']),
+  }),
+});
+const updateProductValidationSchema = z.object({
+  body: z.object({
+    name: z.string({ required_error: 'Name is required.' }),
+    price: z.number({ required_error: 'Price is required.' }),
+    quantity: z.number({ required_error: 'Price is required.' }),
+    types: z.string(),
+    brand: z.string(),
+    size: z.enum(['small', 'medium', 'large']),
+    color: z.string(),
+    condition: z.enum(['new', 'used']),
+  }),
 });
 
 export const ProductValidations = {
-  productValidationSchema,
+  createProductValidationSchema,
+  updateProductValidationSchema,
 };
